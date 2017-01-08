134e13089a36c3d700506055a0a7352bc9f61ceb
Ravi Tiwari <ravi.tiwari@tothenew.com>
2017-01-08 15:44:42 +0530

    Notes about data types

--- src/app/primitives.js
+++ src/app/primitives.js
@@ -1,11 +1,22 @@
-// creating types
+
+console.log('To find the value type, we have the typeof operator');
 
 var primitive_undefined;
-console.log('Every declared uninitialized variable holds the value : ', primitive_undefined);
+var null_value = null;
+var str = 'sample string ::: ';
+var number = 1;
+var bool = true;
+var obj = {};
+var ary = [];
+var fn = function(){};
+var today = new Date;
 
-
-console.log('If we try access a variable that is not declared, we get : ');
-console.log(undeclared_variable);
-
-
-
+console.log(primitive_undefined, typeof primitive_undefined);
+console.log(null_value, typeof null_value);
+console.log(str, typeof str);
+console.log(number, typeof number);
+console.log(bool, typeof bool);
+console.log(obj, typeof obj);
+console.log(ary, typeof ary);
+console.log(fn, typeof fn);
+console.log(today, typeof today);
