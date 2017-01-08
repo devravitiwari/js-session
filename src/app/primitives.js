d3f1f72c6b13fdf20d8f13a98c99ed67100c01c8
Ravi Tiwari <ravi.tiwari@tothenew.com>
2017-01-08 17:02:50 +0530

    Number data type

--- src/app/primitives.js
+++ src/app/primitives.js
@@ -1,22 +1,37 @@
 
-console.log('To find the value type, we have the typeof operator');
+console.log('Number type in JS');
+console.log('===============================');
 
-var primitive_undefined;
-var null_value = null;
-var str = 'sample string ::: ';
-var number = 1;
-var bool = true;
-var obj = {};
-var ary = [];
-var fn = function(){};
-var today = new Date;
+var decimal = 987;
+var octal = 0100;
+var hex = 0xCAFE;
 
-console.log(primitive_undefined, typeof primitive_undefined);
-console.log(null_value, typeof null_value);
-console.log(str, typeof str);
-console.log(number, typeof number);
-console.log(bool, typeof bool);
-console.log(obj, typeof obj);
-console.log(ary, typeof ary);
-console.log(fn, typeof fn);
-console.log(today, typeof today);
+console.log('Decimal 987 in decimal = ', decimal);
+console.log('Octal 0100 in decimal = ', octal);
+console.log('Hex 0xCAFE in decimal = ', hex);
+
+console.log('we have scientific notation for numbers as well.');
+var speedOfLight = 2.99E8;
+var planckConstant = 6.62607004e-34;
+console.log('speedOfLight ', speedOfLight);
+console.log('planckConstant ', planckConstant);
+
+
+console.log('Numbers can be converted to and from from one base to another');
+
+var number = 1024;
+console.log('%d in decimal %s', number, number.toString());
+console.log('%d in binary %s', number,number.toString(2));
+console.log('%d in octal %s', number,number.toString(8));
+console.log('%d in hex %s', number,number.toString(16));
+
+console.log('Decimal %s in decimal %d', '48', parseInt(48));
+console.log('Binary %s in decimal %d', '100101', parseInt('100101', 2));
+console.log('Octal %s in decimal %d', '0100', parseInt('100', 8));
+console.log('Hex %s in decimal %d', '0xCAFE', parseInt('CAFE', 16));
+
+
+
+
+
+
