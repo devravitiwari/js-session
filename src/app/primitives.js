5dfbdbf43ef87adb106e0fe460e014bf560071f9
Ravi Tiwari <ravi.tiwari@tothenew.com>
2017-01-08 15:42:31 +0530

    Notes about data types

--- src/app/primitives.js
+++ src/app/primitives.js
@@ -3,16 +3,9 @@
 var primitive_undefined;
 console.log('Every declared uninitialized variable holds the value : ', primitive_undefined);
 
-var null_value = null;
-console.log(null_value, 'represents intentional absense of Object value');
 
-var str = 'sample string value';
-var number = 1;
-var bool = true;
-var obj = {};
-var ary = [];
-var fn = function(){};
-var today = new Date;
+console.log('If we try access a variable that is not declared, we get : ');
+console.log(undeclared_variable);
 
-console.log(str, number, bool, obj, ary, fn, today);
+
 
