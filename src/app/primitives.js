f4176cc12926a6c0e95492544350cba228b30389
Ravi Tiwari <ravi.tiwari@tothenew.com>
2017-01-08 17:10:33 +0530

    exercise

--- src/app/primitives.js
+++ src/app/primitives.js
@@ -1,37 +1,20 @@
-
-console.log('Number type in JS');
-console.log('===============================');
-
-var decimal = 987;
-var octal = 0100;
-var hex = 0xCAFE;
-
-console.log('Decimal 987 in decimal = ', decimal);
-console.log('Octal 0100 in decimal = ', octal);
-console.log('Hex 0xCAFE in decimal = ', hex);
-
-console.log('we have scientific notation for numbers as well.');
-var speedOfLight = 2.99E8;
-var planckConstant = 6.62607004e-34;
-console.log('speedOfLight ', speedOfLight);
-console.log('planckConstant ', planckConstant);
+/**
+ * Write a function that takes color in hex code '#FFFFFF'
+ * and return equivalent color in rgb as a string - 'rgb(255, 255, 255)'
+ * @param colorInHexCode
+ * @returns {string}
+ */
+function toRGB(colorInHexCode) {
+    return '';
+}
 
 
-console.log('Numbers can be converted to and from from one base to another');
-
-var number = 1024;
-console.log('%d in decimal %s', number, number.toString());
-console.log('%d in binary %s', number,number.toString(2));
-console.log('%d in octal %s', number,number.toString(8));
-console.log('%d in hex %s', number,number.toString(16));
-
-console.log('Decimal %s in decimal %d', '48', parseInt(48));
-console.log('Binary %s in decimal %d', '100101', parseInt('100101', 2));
-console.log('Octal %s in decimal %d', '0100', parseInt('100', 8));
-console.log('Hex %s in decimal %d', '0xCAFE', parseInt('CAFE', 16));
-
-
-
-
-
-
+/**
+ * Write a function that takes color in rgb as a string - 'rgb(255, 255, 255)'
+ * and return equivalent color in hex code '#FFFFFF'
+ * @param colorInHexCode
+ * @returns {string}
+ */
+function toHex(colorInRGBCode) {
+    return '';
+}
