1cc9cd9a87ff0085dc677a0c3583f688f3ed1088
Ravi Tiwari <ravi.tiwari@tothenew.com>
2017-01-08 15:41:48 +0530

    Notes about data types

---
+++ src/app/primitives.js
@@ -0,0 +1,18 @@
+// creating types
+
+var primitive_undefined;
+console.log('Every declared uninitialized variable holds the value : ', primitive_undefined);
+
+var null_value = null;
+console.log(null_value, 'represents intentional absense of Object value');
+
+var str = 'sample string value';
+var number = 1;
+var bool = true;
+var obj = {};
+var ary = [];
+var fn = function(){};
+var today = new Date;
+
+console.log(str, number, bool, obj, ary, fn, today);
+
